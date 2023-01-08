80641f893d0a80ae644440b98f1710ce9c667f3e
Zcodming <Zcodming@users.noreply.github.com>
2023-01-08 16:29:06 +0700

    vite build

---
+++ public/build/assets/ApplicationLogo.946f9fd5.js
@@ -0,0 +1,1 @@
+import{a as t}from"./app.c5fb3a84.js";function g({className:o}){return t("img",{className:o,src:"/storage/logo/Logo-Untan.png",alt:"Logo"})}export{g as A};
--- public/build/assets/InputError.8026f664.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./app.09040f3b.js";function o({message:r,className:t=""}){return r?n("p",{className:"text-sm text-red-600 "+t,children:r}):null}export{o as I};
